825469d7ca50e89f8fe51e42dc0aead7c5cc0e0b
zhipengwu90 <zhipeng.wu@itas.ca>
2023-01-02 15:29:27 -0800

    fix MainNavigation.module

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "13bbeeb33bdab0b6"; }
+/******/ 		__webpack_require__.h = function() { return "40d296a7ff882af6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
