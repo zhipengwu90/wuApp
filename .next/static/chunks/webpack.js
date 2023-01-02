259a9218f7fa6be75a50728f192041149fd41419
zhipengwu90 <zhipeng.wu@itas.ca>
2023-01-02 15:25:40 -0800

    fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "17790f2ed573bcd1"; }
+/******/ 		__webpack_require__.h = function() { return "4eeb9539be5ad620"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
