b9a0e67148ac76d50c0084c206d2daadea0f7f5d
zhipengwu90 <zhipeng.wu@itas.ca>
2023-01-03 14:05:53 -0800

    heroes animation

--- components/Layout/Heroes.js
+++ components/Layout/Heroes.js
@@ -5,27 +5,31 @@ function Heroes() {
     return (
         <div className={classes.heroes}>
             <span className={classes.helloWorld}>Hello World.</span>
-            <div className='container'>
-                <div className={classes.heroesWrap}>
-                    <div className={classes.mainText}>
-                        <span className={classes.wu}>WU.</span>
-                        <span className={classes.with}>with</span>
-                        <span className={classes.developer}>Developer</span>
-                        <div className={classes.box1}></div>
-                        <div className={classes.box2}></div>
-                    </div>
-                    <div className={classes.heroesImg}>
-                        <Image src={`/images/heroes_background.JPG`} alt='heroImg' width={500} height={290}></Image>
-                    </div>
-                </div>
-                <div className={classes.update}>
-                    <span className={classes.updateText}>Updating...</span>
-                    <div className={classes.bar}>
-                        <div className={classes.barRed}> </div>
-                    </div>
 
-                </div>
+            <div className='container fadeIn'>
+   
+                    <div className={classes.heroesWrap}>
+                        <div className={classes.mainText}>
+                            <span className={classes.wu}>WU.</span>
+                            <span className={classes.with}>with</span>
+                            <span className={classes.developer}>Developer</span>
+                            <div className={classes.box1}></div>
+                            <div className={classes.box2}></div>
+                        </div>
+                        <div className={classes.heroesImg}>
+                            <Image src={`/images/heroes_background.JPG`} alt='heroImg' width={500} height={290}></Image>
+                        </div>
+                    </div>
+                    <div className={classes.update}>
+                        <span className={classes.updateText}>Hello World...</span>
+                        <div className={classes.bar}>
+                            <div className={classes.barRed}> </div>
+                        </div>
+
+                    </div>
+         
             </div>
+
         </div>
     )
 }
